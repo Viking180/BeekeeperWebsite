7b83ae3917caeea01226f7f0fa97d606d9b0b3f8
Rhuamer <77104420+Rhuamer@users.noreply.github.com>
2021-03-14 00:22:00 +0100

    Fixed News and Scrollbehaviour
    News:
    -Show text on hover
    -Size, position, style
    
    Scrollbehaviour:
    -Scroll in iframe
    -use right Scrollheight

--- jsScript.js
+++ jsScript.js
@@ -1,7 +1,14 @@
 function openPage(link){
     window.location.href = link;
-
 }
+
 function setURL(url){
     document.getElementById('iframe').src = url;
+    resizeIframe();
+}
+
+function resizeIframe(){
+    var iframe = document.getElementById("iframe");
+    console.log(iframe.contentWindow.document.body.offsetHeight);
+    iframe.style.height = (iframe.contentWindow.document.body.offsetHeight) + "px";
 }
